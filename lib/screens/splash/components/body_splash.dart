145e1adf233adc1d2da70e5c26c94724502fe653
Bui Duc Lam <lam.bd@neo-lab.vn>
2021-07-31 00:50:28 +0700

    add splash

---
+++ lib/screens/splash/components/body_splash.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BodySplash extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<BodySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column());
+  }
+}
